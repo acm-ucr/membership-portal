dc361874a51ce9e054a1f8184ee2ac8087a186f6
Rayyan Zaid <rayyanzaid0401@gmail.com>
2022-10-01 16:15:59 -0700

    Home Component

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
-const Home = () => {
-  return <div>Home</div>;
+const Home = ({name,points,year}) => {
+  return (
+    <div className="ml-12 mt-20">
+      <div className="mb-4">
+        <h1 className="text-8xl text-black font-bold h-min w-min">welcome‚</h1>
+        <h1 className="text-8xl text-black font-bold">{name}</h1>
+      </div>
+
+      <div>
+        <h5 className="text-4xl text-gray-500 text-acm-gray font-acm ">member since {year} | {points} points</h5>
+      </div>
+    </div>
+  )
 };
 
+
+Home.defaultProps = {
+  name: "User",
+  points: 0,
+  year: 2022
+
+}
 export default Home;
