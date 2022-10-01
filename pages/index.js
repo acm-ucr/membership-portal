a36f3ceabbf0cb07ca4ef6dc66bd2e94c9d5ea72
theo <theobfernandez@gmail.com>
2022-10-01 13:51:46 -0700

    remove test cases

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,7 @@
 import React from "react";
-import Announcement from "../components/Announcement";
-import { Row } from "react-bootstrap";
 
 const index = () => {
-  return (
-    <div>
-      <Row className="p-5">
-        <Announcement
-          color="#c3e88d"
-          title="really cool title"
-          date="january 1st"
-          time="1 pm"
-          location="winston"
-        />
-      </Row>
-    </div>
-  );
+  return <div>testing</div>;
 };
 
 export default index;
