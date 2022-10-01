0ab11b635a2c59dc69e4ce72778152496814fe50
theo <theobfernandez@gmail.com>
2022-10-01 13:47:14 -0700

    run prettier

--- components/Announcement.jsx
+++ components/Announcement.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import Card from "react-bootstrap/Card";
 
-const Announcement = () => {
-  return <div>Announcement</div>;
+// doesn't display when I import the component into index for some reason
+
+const Announcement = ({ color, title, date, time, location }) => {
+  return (
+    <div>
+      <Card
+        className="w-fit h-fit !border-none !rounded-3xl font-lexend"
+        style={{
+          backgroundImage: `linear-gradient(to right,
+         ${color} 0% 6%,
+        black 6% 7.5%,
+         white 7.5% 100%)`,
+        }}
+      >
+        <Card.Body className="ml-8 flex-col justify-evenly">
+          <p className="text-4xl font-bold text-acm-black">{title}</p>
+          <div className="flex mb-2">
+            <div
+              className={`w-6 h-6 self-center flex-none rounded-full mr-2`}
+              style={{ backgroundColor: `${color}` }}
+            ></div>
+            <div className="text-acm-black text-xl">date: {date}</div>
+          </div>
+          <div className="flex mb-2">
+            <div
+              className={`w-6 h-6 self-center flex-none rounded-full mr-2`}
+              style={{ backgroundColor: `${color}` }}
+            ></div>
+            <div className="text-acm-black text-xl">time: {time}</div>
+          </div>
+          <div className="flex">
+            <div
+              className={`w-6 h-6 self-center flex-none rounded-full mr-2`}
+              style={{ backgroundColor: `${color}` }}
+            ></div>
+            <div className="text-acm-black text-xl">location: {location}</div>
+          </div>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 };
 
 export default Announcement;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,21 @@
 import React from "react";
+import Announcement from "../components/Announcement";
+import { Row } from "react-bootstrap";
 
 const index = () => {
-  return <div>testing</div>;
+  return (
+    <div>
+      <Row className="p-5">
+        <Announcement
+          color="#c3e88d"
+          title="really cool title"
+          date="january 1st"
+          time="1 pm"
+          location="winston"
+        />
+      </Row>
+    </div>
+  );
 };
 
 export default index;
