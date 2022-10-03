c9d3866d9056d46a9124f0c437de3dc9913d33cc
Jyro888 <92472721+Jyro88@users.noreply.github.com>
2022-10-02 23:50:22 -0700

    accidentally removed and readded form.jsx

---
+++ components/Form.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <div className="!rounded-3xl bg-red-500">
+      <div>
+        <div></div>
+      </div>
+      <div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Form;
