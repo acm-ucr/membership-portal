1b80a74f948a1eb0bf73a3fc45412e92ea66db5b
Kninja99 <kcdamschen@yahoo.com>
2022-10-03 15:40:01 -0700

    init commit for welcome component
    * added basic elements and simple styles to start
    * need to still polish this portion up

--- components/Welcome.jsx
+++ components/Welcome.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Image from 'next/image'
 
 const Welcome = () => {
-  return <div>Welcome</div>;
+  return (
+  <div className="flex flex-col justify-center items-center">
+    <h1>Welcome to</h1>
+    <Image className="bg-[#86AFF9] rounded-md py-8 w-auto h-36" src="/acmucr.png" alt="ACM @ UCR" width={681}
+        height={134}/>
+  </div>);
 };
 
 export default Welcome;
