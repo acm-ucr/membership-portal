e3b9e6925d5ee4019a3a09da370c2b6521d1b2f2
Kninja99 <kcdamschen@yahoo.com>
2022-10-03 15:45:08 -0700

    Formated version
    * formated version of the previous commit

--- components/Welcome.jsx
+++ components/Welcome.jsx
@@ -1,13 +1,19 @@
 import React from "react";
-import Image from 'next/image'
+import Image from "next/image";
 
 const Welcome = () => {
   return (
-  <div className="flex flex-col justify-center items-center">
-    <h1>Welcome to</h1>
-    <Image className="bg-[#86AFF9] rounded-md py-8 w-auto h-36" src="/acmucr.png" alt="ACM @ UCR" width={681}
-        height={134}/>
-  </div>);
+    <div className="flex flex-col justify-center items-center">
+      <h1>Welcome to</h1>
+      <Image
+        className="bg-[#86AFF9] rounded-md py-8 w-auto h-36"
+        src="/acmucr.png"
+        alt="ACM @ UCR"
+        width={681}
+        height={134}
+      />
+    </div>
+  );
 };
 
 export default Welcome;
