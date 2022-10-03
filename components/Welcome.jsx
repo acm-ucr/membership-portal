26197e4e5b1ec31fe2c4799161d92863f94a09e3
Kninja99 <kcdamschen@yahoo.com>
2022-10-03 16:28:12 -0700

    More styling updates to welcome comp
    * added text styling and some resonsive styles, this might need to be
      changed once we get the responsive style down for the official page,
      but for now its for when the component stays on half of the page
    * removed styles being over written in the Image element

--- components/Welcome.jsx
+++ components/Welcome.jsx
@@ -4,9 +4,11 @@ import Image from "next/image";
 const Welcome = () => {
   return (
     <div className="flex flex-col justify-center items-center">
-      <h1>Welcome to</h1>
+      <h1 className="text-[#0F121A] font-bold xl:text-8xl md:text-6xl sm:text-5xl text-2xl">
+        Welcome to
+      </h1>
       <Image
-        className="bg-[#86AFF9] rounded-md py-8 w-auto h-36"
+        className="bg-[#86AFF9] rounded-3xl"
         src="/acmucr.png"
         alt="ACM @ UCR"
         width={681}
