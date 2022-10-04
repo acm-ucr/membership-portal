e80c46fdd9cc15f95875f576cab9beafd094673a
Kninja99 <kcdamschen@yahoo.com>
2022-10-03 21:32:46 -0700

    Qucik max width update
    * sorry forgot to add my max width change before pr

--- components/Welcome.jsx
+++ components/Welcome.jsx
@@ -7,7 +7,7 @@ const Welcome = () => {
       <h1 className="text-[#0F121A] text-center font-bold xl:text-7xl l:text-6xl s:text-7xl text-5xl">
         Welcome to
       </h1>
-      <div className="bg-[#86AFF9] rounded-3xl w-10/12 min-w-[300px] max-w-[680px] mx-auto my-3 px-6">
+      <div className="bg-[#86AFF9] rounded-3xl w-10/12 min-w-[300px] max-w-[450px] mx-auto my-3 px-6">
         <Image
           layout="responsive"
           src="/acmucr.png"
