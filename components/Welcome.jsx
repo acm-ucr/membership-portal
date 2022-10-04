d4be249566acda73704c888b7326f20025875ce9
Kninja99 <kcdamschen@gmail.com>
2022-10-03 21:39:39 -0700

    Color mapping update with tailwind
    * updated colors to use tailwind mapping

--- components/Welcome.jsx
+++ components/Welcome.jsx
@@ -4,10 +4,10 @@ import Image from "next/image";
 const Welcome = () => {
   return (
     <div>
-      <h1 className="text-[#0F121A] text-center font-bold xl:text-7xl l:text-6xl s:text-7xl text-5xl">
+      <h1 className="text-acm-black text-center font-bold xl:text-7xl l:text-6xl s:text-7xl text-5xl">
         Welcome to
       </h1>
-      <div className="bg-[#86AFF9] rounded-3xl w-10/12 min-w-[300px] max-w-[450px] mx-auto my-3 px-6">
+      <div className="bg-acm-blue rounded-3xl w-10/12 min-w-[300px] max-w-[450px] mx-auto my-3 px-6">
         <Image
           layout="responsive"
           src="/acmucr.png"
