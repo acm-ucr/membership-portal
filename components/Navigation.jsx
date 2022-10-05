8af6149e4de131396adba421d30d198fa20013e6
Arnav-Menon <arnavmenon@gmail.com>
2022-10-04 22:09:13 -0700

    first commit, pretty ugly ngl

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Navigation = () => {
-  return <div>Navigation</div>;
+  return (
+    <div>
+      <img src={"../public/vercel.svg"} alt="acm@ucr" style={{float:"left"}}/>
+      <h3 style={{float:"left", paddingLeft:"2%"}}>member portal</h3>
+      <h4 style={{float:"right", wordSpacing:"15px", paddingRight:"2%"}}>profile forms resources "general"</h4>
+    </div>
+  );
 };
 
 export default Navigation;
