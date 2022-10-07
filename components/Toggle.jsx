2944711fb7eb78167c4731f10cbbd7722e00e0ae
michael07w <michaelwessels207@gmail.com>
2022-10-06 22:06:58 -0700

    Implemented General and Admin badges

--- components/Toggle.jsx
+++ components/Toggle.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { ToggleButton } from "react-bootstrap";
 
 const Toggle = () => {
-  return <div>Toggle</div>;
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  useEffect(() => {
+    const name = isAdmin ? "Admin" : "General";
+    console.log(`You are currently ${name}`);
+  });
+
+  // ToggleButton is used to update the state variable `isAdmin`, which updates the icon displayed
+  return (
+    <div>
+      <ToggleButton
+        id="toggle-check"
+        type="checkbox"
+        variant="outline-primary"
+        checked={isAdmin}
+        onChange={() => {
+          setIsAdmin(!isAdmin);
+        }}
+      >
+        Status
+      </ToggleButton>
+      {isAdmin ? (
+        <h6 className="flex float-right rounded-full border-solid border-2 border-black pb-1 px-1 bg-black text-white">
+          admin.
+          <div className="bg-white rounded-full ml-2 mt-[3px] h-4 w-4"></div>
+        </h6>
+      ) : (
+        <h6 className="float-right rounded-full border-solid border-2 border-black pb-1 px-1">
+          &#11044; general
+        </h6>
+      )}
+    </div>
+  );
 };
 
 export default Toggle;
--- pages/index.js
+++ pages/index.js
@@ -17,3 +17,4 @@ const index = () => {
 };
 
 export default index;
+
