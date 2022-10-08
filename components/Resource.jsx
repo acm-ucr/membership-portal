f84ad9dd25191eb913cd3d02bf4c7edaf3f0094e
wmx-OwO <wumengxuan0728@gmail.com>
2022-10-07 19:43:29 -0700

    formatting

--- components/Resource.jsx
+++ components/Resource.jsx
@@ -1,38 +1,58 @@
 import React from "react";
-import {FaFileAlt, FaYoutube} from "react-icons/fa"
-import {FaGithub} from "react-icons/fa"
+import { FaFileAlt, FaYoutube } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 
+const Resource = ({ titles, documentLink, youtubeLink, githubLink }) => {
+  return (
+    <span className="rounded-3xl aspect-[1/1] w-full flex  bg-acm-white text-acm-white border-4 border-acm-black">
+      <span className="relative -left-4 top-4 rounded-3xl aspect-[1/1] w-full block items-center bg-acm-black">
+        <div className=" flex w-full flex-col h-full justify-between p-3 md:p-5">
+          <div></div>
+          <p className="text-center text-acm-blue font-lexend font-medium text-2xl">
+            {titles}
+          </p>
+          <div className="flex items-center justify-center ">
+            {documentLink && (
+              <a
+                href={documentLink}
+                target="_blank"
+                className="inline p-2"
+                rel="noreferrer"
+              >
+                <FaFileAlt className="text-4xl text-acm-white hover:text-acm-blue" />
+              </a>
+            )}
 
-const Resource = ({titles, documentLink, youtubeLink, githubLink}) =>
-{
-  return( 
-  <span className="rounded-3xl aspect-[1/1] w-full flex  bg-acm-white text-acm-white border-4 border-acm-black" >
-    <span className="relative -left-4 top-4 rounded-3xl aspect-[1/1] w-full block items-center bg-acm-black" >
-      <div className=" flex w-full flex-col h-full justify-between p-3 md:p-5">
-        <div></div>
-      <p className="text-center text-acm-blue font-lexend font-medium text-2xl">{titles}</p>
-    <div className="flex items-center justify-center ">
+            {youtubeLink == "" ? (
+              <></>
+            ) : (
+              <a
+                href={youtubeLink}
+                target="_blank"
+                className="inline p-2"
+                rel="noreferrer"
+              >
+                <FaYoutube className="text-4xl text-acm-white hover:text-acm-blue"></FaYoutube>
+              </a>
+            )}
 
-      {
-        documentLink && <a href = {documentLink} target = "_blank" className="inline p-2" rel="noreferrer">
-        <FaFileAlt className="text-4xl text-acm-white hover:text-acm-blue"/></a>
-      }
-
-      {youtubeLink == "" ? <></>:
-      <a href = {youtubeLink} target = "_blank" className="inline p-2" rel="noreferrer">
-        <FaYoutube className="text-4xl text-acm-white hover:text-acm-blue"></FaYoutube>
-      </a>}
-
-      {githubLink == "" ? <></>:
-      <a href = {githubLink} target = "_blank" className="inline p-2" rel="noreferrer">
-        <FaGithub className="text-4xl text-acm-white hover:text-acm-blue"></FaGithub>
-      </a>}
-
-    </div>
-    </div>
+            {githubLink == "" ? (
+              <></>
+            ) : (
+              <a
+                href={githubLink}
+                target="_blank"
+                className="inline p-2"
+                rel="noreferrer"
+              >
+                <FaGithub className="text-4xl text-acm-white hover:text-acm-blue"></FaGithub>
+              </a>
+            )}
+          </div>
+        </div>
+      </span>
     </span>
-  </span>
-  )
-}
+  );
+};
 
 export default Resource;
--- components/Resources.jsx
+++ components/Resources.jsx
@@ -3,52 +3,49 @@ import Resource from "./Resource";
 import TimeFilter from "./TimeFilter";
 import { Col, Row } from "react-bootstrap";
 
-const recourceCardList=[
+const recourceCardList = [
   {
-      title: 'API Workshop',
-      documentlink:"https://www.youtube.com/watch?v=w7ejDZ8SWv8",
-      youtubelink:"https://www.youtube.com/watch?v=w7ejDZ8SWv8",
-      gitlink:"https://www.youtube.com/watch?v=w7ejDZ8SWv8",
+    title: "API Workshop",
+    documentlink: "https://www.youtube.com/watch?v=w7ejDZ8SWv8",
+    youtubelink: "https://www.youtube.com/watch?v=w7ejDZ8SWv8",
+    gitlink: "https://www.youtube.com/watch?v=w7ejDZ8SWv8",
   },
   {
-      title: 'Professional Development Workshop',
-      documentlink:"https://www.youtube.com/watch?v=w7ejDZ8SWv8",
-      youtubelink:"https://www.youtube.com/watch?v=w7ejDZ8SWv8",
-      gitlink:"",
+    title: "Professional Development Workshop",
+    documentlink: "https://www.youtube.com/watch?v=w7ejDZ8SWv8",
+    youtubelink: "https://www.youtube.com/watch?v=w7ejDZ8SWv8",
+    gitlink: "",
   },
   {
-      title: 'Web Development Workshop',
-      documentlink:"https://www.youtube.com/watch?v=w7ejDZ8SWv8",
-      youtubelink:"https://www.youtube.com/watch?v=w7ejDZ8SWv8",
-      gitlink:"https://www.youtube.com/watch?v=w7ejDZ8SWv8",
+    title: "Web Development Workshop",
+    documentlink: "https://www.youtube.com/watch?v=w7ejDZ8SWv8",
+    youtubelink: "https://www.youtube.com/watch?v=w7ejDZ8SWv8",
+    gitlink: "https://www.youtube.com/watch?v=w7ejDZ8SWv8",
   },
   {
-    title: 'Web Development Workshop',
-    documentlink:"https://www.youtube.com/watch?v=w7ejDZ8SWv8",
-    youtubelink:"https://www.youtube.com/watch?v=w7ejDZ8SWv8",
-    gitlink:"https://www.youtube.com/watch?v=w7ejDZ8SWv8",
-}
-] 
+    title: "Web Development Workshop",
+    documentlink: "https://www.youtube.com/watch?v=w7ejDZ8SWv8",
+    youtubelink: "https://www.youtube.com/watch?v=w7ejDZ8SWv8",
+    gitlink: "https://www.youtube.com/watch?v=w7ejDZ8SWv8",
+  },
+];
 
 const Resources = () => {
   return (
-    <div className = "w-11/12 flex items-center flex-col justify-center">
+    <div className="w-11/12 flex items-center flex-col justify-center">
       <TimeFilter />
-        <Row className="w-11/12 items-center ">
-      {recourceCardList.map(
-      (recourceCard,index) => 
-      (
-        <Col className = "p-3" xs = {12} sm = {6} md ={4} lg = {3} key = {index}>
-        <Resource
-                      titles = {recourceCard.title}
-                      documentLink = {recourceCard.documentlink}
-                      youtubeLink = {recourceCard.youtubelink}
-                      githubLink = {recourceCard.gitlink}
-        />
-        </Col>
-      )
-    )}
-  </Row>
+      <Row className="w-11/12 items-center ">
+        {recourceCardList.map((recourceCard, index) => (
+          <Col className="p-3" xs={12} sm={6} md={4} lg={3} key={index}>
+            <Resource
+              titles={recourceCard.title}
+              documentLink={recourceCard.documentlink}
+              youtubeLink={recourceCard.youtubelink}
+              githubLink={recourceCard.gitlink}
+            />
+          </Col>
+        ))}
+      </Row>
     </div>
   );
 };
