8087f13d273e91241a8c51ab9950c1e84e0d20e2
Divyank Shah <divyank.shah.2016@gmail.com>
2022-10-08 12:35:59 -0700

    finalize

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -7,62 +7,53 @@ const editProfile = () => {
 
 const Profile = ({ name, major, classOf, netId, email, points }) => {
   return (
-    <div>
-      <Row className="w-2/5">
-        <Col className="w-2/3">
-          <p className="text-acm-black text-3xl font-lexend h-fit w-fit font-bold pb-1">
-            name:
-          </p>
-          <p className="text-acm-black text-2xl font-lexend h-fit w-fit pb-1">
-            {name}
-          </p>
+    <Row className="w-full">
+      <Col xl={6}>
+        <p className="text-acm-black text-3xl font-lexend font-bold pb-1">
+          name:
+        </p>
+        <p className="text-acm-black text-2xl font-lexend pb-1">{name}</p>
 
-          <p className="text-acm-black text-3xl font-lexend h-fit w-fit font-bold">
-            major:
-          </p>
-          <p className="text-acm-black text-2xl font-lexend h-fit w-fit ">
-            {major}
-          </p>
+        <p className="text-acm-black text-3xl font-lexend font-bold">major:</p>
+        <p className="text-acm-black text-2xl font-lexend">{major}</p>
 
-          <p className="text-acm-black text-3xl font-lexend h-fit w-fit font-bold">
-            class of:
-          </p>
-          <p className="text-acm-black text-2xl font-lexend h-fit w-fit">
-            {classOf}
-          </p>
+        <p className="text-acm-black text-3xl font-lexend font-bold">
+          class of:
+        </p>
+        <p className="text-acm-black text-2xl font-lexend">{classOf}</p>
+      </Col>
+      <Col xl={6}>
+        <p className="text-acm-black text-3xl font-lexend h-fit w-fit font-bold">
+          netid:
+        </p>
+        <p className="text-acm-black text-2xl font-lexend h-fit w-fit">
+          {netId}
+        </p>
 
-          <button
-            id="editProfile"
-            className=" mt-8 w-5/6 h-1/6 font-lexend font-bold text-acm-black text-2xl transition-colors duration-150 bg-acm-lightpurple rounded-lg focus:shadow-outline hover:bg-indigo-500"
-            onClick={editProfile}
-          >
-            edit profile
-          </button>
-        </Col>
-        <Col className="w-2/3">
-          <p className="text-acm-black text-3xl font-lexend h-fit w-fit font-bold">
-            netid:
-          </p>
-          <p className="text-acm-black text-2xl font-lexend h-fit w-fit ">
-            {netId}
-          </p>
+        <p className="text-acm-black text-3xl font-lexend h-fit w-fit font-bold">
+          email:
+        </p>
+        <p className="text-acm-black text-2xl font-lexend h-fit w-fit">
+          {email}
+        </p>
 
-          <p className="text-acm-black text-3xl font-lexend h-fit w-fit font-bold">
-            email:
-          </p>
-          <p className="text-acm-black text-2xl font-lexend h-fit w-fit">
-            {email}
-          </p>
-
-          <p className="text-acm-black text-3xl font-lexend h-fit w-fit font-bold">
-            points:
-          </p>
-          <p className="text-acm-black text-2xl font-lexend h-fit w-fit font-bold">
-            {points}
-          </p>
-        </Col>
-      </Row>
-    </div>
+        <p className="text-acm-black text-3xl font-lexend h-fit w-fit font-bold">
+          points:
+        </p>
+        <p className="text-acm-black text-2xl font-lexend h-fit w-fit">
+          {points}
+        </p>
+      </Col>
+      <Col xl={12}>
+        <button
+          id="editProfile"
+          className=" w-full sm:w-4/6 md:w-5/12 py-2 font-lexend font-bold text-acm-black text-2xl transition-colors duration-150 bg-acm-lightpurple rounded-lg focus:shadow-outline hover:bg-indigo-500"
+          onClick={editProfile}
+        >
+          edit profile
+        </button>
+      </Col>
+    </Row>
   );
 };
 
