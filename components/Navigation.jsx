c9af840832fab212425dda196af8a137b2b5b6c3
Divyank Shah <divyank.shah.2016@gmail.com>
2022-10-08 11:38:39 -0700

    finalize

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,18 +1,70 @@
 import React from "react";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Link from "next/link";
+import Image from "next/image";
 
 const Navigation = () => {
   return (
-    <div className="flow-root">
-      <h3 className="float-left px-3 pt-3">member portal</h3>
-      <div className="float-right space-x-10 text-acm-black px-3 pt-3">
-        <h4 className="float-left rounded hover:shadow-lg">profile</h4>
-        <h4 className="float-left rounded hover:shadow-lg">forms</h4>
-        <h4 className="float-left rounded hover:shadow-lg">resources</h4>
-        <h6 className="float-left rounded hover:shadow-lg border-solid border-2 border-black">
-          general placeholder
-        </h6>
-      </div>
-    </div>
+    <Navbar
+      collapseOnSelect
+      className=" py-0 font-lexend w-full px-3 m-0 h-[12vh] bg-white border-acm-blue border-b-4 flex justify-center items-center"
+      expand="md"
+      fixed="top"
+    >
+      <Navbar.Brand>
+        <Link href="/dashboard" passHref>
+          <Nav.Link eventKey="1">
+            <Image
+              src="/acm-ucr-logo.png"
+              alt="ACM at UCR"
+              width={100}
+              height={100}
+            />
+          </Nav.Link>
+        </Link>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="navbar-nav" />
+      <Navbar.Collapse
+        id="navbar-nav"
+        className="-mt-2 flex justify-center items-center"
+      >
+        <Nav className=" w-12/12 no-underline ml-auto text-2xl">
+          <Link href="/dashboard" passHref>
+            <Nav.Link
+              className="m-auto whitespace-nowrap w-full text-center !text-acm-black !font-medium hover:!text-acm-blue"
+              eventKey="6"
+            >
+              dashboard
+            </Nav.Link>
+          </Link>
+          <Link href="/profile" passHref>
+            <Nav.Link
+              className="m-auto whitespace-nowrap w-full text-center !text-acm-black !font-medium hover:!text-acm-blue"
+              eventKey="2"
+            >
+              profile
+            </Nav.Link>
+          </Link>
+          <Link href="/forms" passHref>
+            <Nav.Link
+              className="m-auto whitespace-nowrap w-full text-center !text-acm-black !font-medium hover:!text-acm-blue"
+              eventKey="3"
+            >
+              forms
+            </Nav.Link>
+          </Link>
+          <Link href="/resources" passHref>
+            <Nav.Link
+              className="m-auto whitespace-nowrap w-full text-center !text-acm-black !font-medium hover:!text-acm-blue"
+              eventKey="4"
+            >
+              resources
+            </Nav.Link>
+          </Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
