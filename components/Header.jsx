a0c8beb767144ae5de06203245a111a5c18094c8
wmx-OwO <wumengxuan0728@gmail.com>
2022-10-08 17:07:47 -0700

    Add Background Color Parameter to Header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-const Header = ({ title }) => {
+const Header = ({ title, color ="acm-black"}) => {
   return (
-    <div className="w-11/12 p-2 font-lexend font-semibold rounded-3 text-2xl bg-acm-black text-acm-white">
+    <div className= {`w-11/12 p-2 font-lexend font-semibold rounded-3 text-2xl text-acm-white bg-${color}`}>
       {title}
     </div>
   );
