c00dc0309eebbc4c66d82804283e498a20473472
wmx-OwO <wumengxuan0728@gmail.com>
2022-10-08 21:55:48 -0700

    format

--- components/SubmittedForm.jsx
+++ components/SubmittedForm.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 const Form = ({ title, text, border, background }) => {
   return (
-    <div className={`${background} rounded-3xl flex  items-center flex-col aspect-[1/1]`}>
-        <p className="text-center p-4 font-bold text-xl">{title}</p>
+    <div
+      className={`${background} rounded-3xl flex  items-center flex-col aspect-[1/1]`}
+    >
+      <p className="text-center p-4 font-bold text-xl">{title}</p>
     </div>
   );
 };
