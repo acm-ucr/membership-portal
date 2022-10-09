bdd41e9d823ad5da86284045f42624488dd22a2a
Raidah Fairooz <rfair008@ucr.edu>
2022-10-09 16:10:44 -0700

    added no meetings code.

--- components/Meetings.jsx
+++ components/Meetings.jsx
@@ -1,24 +1,41 @@
 import React from "react";
 import Meeting from "./Meeting";
 import MeetingsData from "./MeetingsData";
+
 const Meetings = () => {
-  return (
-    <div className="flex flex-col font-lexend border-black border-2 rounded-3xl mt-3">
-      <div className="flex bg-black text-white text-2xl font-semibold rounded-3xl p-1 px-4 w-fit mx-4 mt-4 mb-4">
-        scheduled meetings
+  const data = MeetingsData;
+  if (data == 0) {
+    return (
+      <div className="flex flex-col font-lexend border-black border-2 rounded-3xl mt-3 min-h-fit h-2/6">
+        <div className="flex bg-black text-white text-2xl font-semibold rounded-3xl p-1 px-4 w-fit mx-4 mt-4 mb-4">
+          scheduled meetings
+        </div>
+        <div className="flex font-lexend justify-center items-center py-16">
+          {" "}
+          currently no scheduled meetings{" "}
+        </div>
       </div>
-      {MeetingsData.map((meeting, value) => (
-        <Meeting
-          key={value}
-          title={meeting.title}
-          date={meeting.date}
-          time={meeting.time}
-          location={meeting.location}
-          backgroundColor={meeting.backgroundColor}
-        />
-      ))}
-    </div>
-  );
+    );
+  }
+  {
+    return (
+      <div className="flex flex-col font-lexend border-black border-2 rounded-3xl mt-3">
+        <div className="flex bg-black text-white text-2xl font-semibold rounded-3xl p-1 px-4 w-fit mx-4 mt-4 mb-4">
+          scheduled meetings
+        </div>
+        {MeetingsData.map((meeting, value) => (
+          <Meeting
+            key={value}
+            title={meeting.title}
+            date={meeting.date}
+            time={meeting.time}
+            location={meeting.location}
+            backgroundColor={meeting.backgroundColor}
+          />
+        ))}
+      </div>
+    );
+  }
 };
 
 export default Meetings;
