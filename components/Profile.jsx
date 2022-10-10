55e8e873bcb747941c18b9a2d62784c4c14d81ac
Rayyan Zaid <rayyanzaid0401@gmail.com>
2022-10-09 20:30:50 -0700

    initial commit

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,60 +1,172 @@
-import React from "react";
+import React, { useState } from "react";
 import { Col, Row } from "react-bootstrap";
 
-const editProfile = () => {
-  console.log("Edit Profile Button Pressed");
-};
-
 const Profile = ({ name, major, classOf, netId, email, points }) => {
-  return (
-    <Row className="w-full">
-      <Col xl={6}>
-        <p className="text-acm-black text-3xl font-lexend font-bold pb-1">
-          name:
-        </p>
-        <p className="text-acm-black text-2xl font-lexend pb-1">{name}</p>
+  const [edit, setEdit] = useState(false);
 
-        <p className="text-acm-black text-3xl font-lexend font-bold">major:</p>
-        <p className="text-acm-black text-2xl font-lexend">{major}</p>
+  // Change name
+  const [newName, setName] = useState(name);
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+    // console.log(event.target.value);
+  };
 
-        <p className="text-acm-black text-3xl font-lexend font-bold">
-          class of:
-        </p>
-        <p className="text-acm-black text-2xl font-lexend">{classOf}</p>
-      </Col>
-      <Col xl={6}>
-        <p className="text-acm-black text-3xl font-lexend h-fit w-fit font-bold">
-          netid:
-        </p>
-        <p className="text-acm-black text-2xl font-lexend h-fit w-fit">
-          {netId}
-        </p>
+  // Change major
+  // const [newMajor, setMajor] = useState(major);
+  // const handleMajorChange = event => {
+  //   setMajor(event.target.value);
+  //   // console.log(event.target.value);
+  // };
 
-        <p className="text-acm-black text-3xl font-lexend h-fit w-fit font-bold">
-          email:
-        </p>
-        <p className="text-acm-black text-2xl font-lexend h-fit w-fit">
-          {email}
-        </p>
+  // Change class of
 
-        <p className="text-acm-black text-3xl font-lexend h-fit w-fit font-bold">
-          points:
-        </p>
-        <p className="text-acm-black text-2xl font-lexend h-fit w-fit">
-          {points}
-        </p>
-      </Col>
-      <Col xl={12}>
-        <button
-          id="editProfile"
-          className=" w-full sm:w-4/6 md:w-5/12 py-2 font-lexend font-bold text-acm-black text-2xl transition-colors duration-150 bg-acm-lightpurple rounded-lg focus:shadow-outline hover:bg-indigo-500"
-          onClick={editProfile}
-        >
-          edit profile
-        </button>
-      </Col>
-    </Row>
-  );
+  // Functions for buttons
+  const editProfile = () => {
+    console.log("Edit Profile Button Pressed");
+    setEdit(true);
+    console.log(name);
+  };
+
+  const saveEditedValues = () => {
+    setEdit(false);
+  };
+
+  const cancelChanges = () => {
+    setName(name);
+    setEdit(false);
+  };
+
+  if (edit) {
+    return (
+      <Row className="w-full">
+        <Col xl={6}>
+          <p className="text-acm-black text-3xl font-lexend font-bold pb-1">
+            name:
+          </p>
+          <input
+            type="text"
+            placeholder={newName}
+            onChange={handleNameChange}
+            className="text-acm-black text-2xl font-lexend pb-1 bg-gray-300 rounded-lg"
+          />
+
+          <p className="text-acm-black text-3xl font-lexend font-bold">
+            major:
+          </p>
+          <input
+            type="text"
+            placeholder={major}
+            className="text-acm-black text-2xl font-lexend pb-1 bg-gray-300 rounded-lg"
+          />
+
+          <p className="text-acm-black text-3xl font-lexend font-bold">
+            class of:
+          </p>
+          <input
+            type="text"
+            placeholder={classOf}
+            className="text-acm-black text-2xl font-lexend pb-1 bg-gray-300 rounded-lg"
+          />
+        </Col>
+        <Col xl={6}>
+          <p className="text-acm-black text-3xl font-lexend h-fit w-fit font-bold">
+            netid:
+          </p>
+          <p className="text-acm-black text-2xl font-lexend h-fit w-fit">
+            {netId}
+          </p>
+
+          <p className="text-acm-black text-3xl font-lexend h-fit w-fit font-bold">
+            email:
+          </p>
+          <p className="text-acm-black text-2xl font-lexend h-fit w-fit">
+            {email}
+          </p>
+
+          <p className="text-acm-black text-3xl font-lexend h-fit w-fit font-bold">
+            points:
+          </p>
+          <p className="text-acm-black text-2xl font-lexend h-fit w-fit">
+            {points}
+          </p>
+        </Col>
+
+        <Row>
+          <Col xl={6}>
+            <button
+              id="editProfile"
+              className="mt-3 w-full sm:w-4/6 md:w-4/5 py-2 font-lexend font-bold text-acm-black text-2xl border-2 border-acm-lightpurple transition-colors duration-150 bg-acm-lightpurple rounded-lg focus:shadow-outline hover:border-acm-black "
+              onClick={saveEditedValues}
+            >
+              save
+            </button>
+          </Col>
+
+          <Col xl={6}>
+            <button
+              id="editProfile"
+              className="mt-3 w-full sm:w-4/6 md:w-4/5 py-2 font-lexend font-bold text-acm-black text-2xl  bg-acm-white border-2 border-acm-black rounded-lg focus:shadow-outline hover:bg-indigo-500"
+              onClick={cancelChanges}
+            >
+              cancel
+            </button>
+          </Col>
+        </Row>
+      </Row>
+    );
+  } else {
+    return (
+      <Row className="w-full">
+        <Col xl={6}>
+          <p className="text-acm-black text-3xl font-lexend font-bold pb-1">
+            name:
+          </p>
+          <p className="text-acm-black text-2xl font-lexend pb-1">{newName}</p>
+
+          <p className="text-acm-black text-3xl font-lexend font-bold">
+            major:
+          </p>
+          <p className="text-acm-black text-2xl font-lexend">{major}</p>
+
+          <p className="text-acm-black text-3xl font-lexend font-bold">
+            class of:
+          </p>
+          <p className="text-acm-black text-2xl font-lexend">{classOf}</p>
+        </Col>
+        <Col xl={6}>
+          <p className="text-acm-black text-3xl font-lexend h-fit w-fit font-bold">
+            netid:
+          </p>
+          <p className="text-acm-black text-2xl font-lexend h-fit w-fit">
+            {netId}
+          </p>
+
+          <p className="text-acm-black text-3xl font-lexend h-fit w-fit font-bold">
+            email:
+          </p>
+          <p className="text-acm-black text-2xl font-lexend h-fit w-fit">
+            {email}
+          </p>
+
+          <p className="text-acm-black text-3xl font-lexend h-fit w-fit font-bold">
+            points:
+          </p>
+          <p className="text-acm-black text-2xl font-lexend h-fit w-fit">
+            {points}
+          </p>
+        </Col>
+        <Col xl={12}>
+          <button
+            id="editProfile"
+            className=" w-full sm:w-4/6 md:w-5/12 py-2 font-lexend font-bold text-acm-black border-2 border-acm-lightpurple text-2xl transition-colors duration-150 bg-acm-lightpurple rounded-lg focus:shadow-outline hover:border-acm-black"
+            onClick={editProfile}
+          >
+            edit profile
+          </button>
+        </Col>
+      </Row>
+    );
+  }
 };
 
 export default Profile;
