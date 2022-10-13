7d88188c8bd3034df40b5b775114ec32ac2bc06f
wmx-OwO <wumengxuan0728@gmail.com>
2022-10-12 18:17:59 -0700

    change font

--- components/Welcome.jsx
+++ components/Welcome.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const Welcome = () => {
   return (
     <div>
-      <h1 className="text-acm-black text-center font-bold xl:text-7xl l:text-6xl s:text-7xl text-5xl">
+      <p className="text-acm-black text-center font-bold xl:text-7xl l:text-6xl s:text-7xl text-5xl">
         Welcome to
-      </h1>
+      </p>
       <p className="bg-acm-blue rounded-3xl text-acm-white text-center font-bold xl:text-7xl l:text-6xl s:text-7xl text-5xl font-athiti p-1 my-4">
-        acm@Ucr
+        acm@ucr
       </p>
     </div>
   );
