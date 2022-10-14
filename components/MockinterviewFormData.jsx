ced959aa9c1d67c74cfd728c8cea871782ab9a9c
wmx-OwO <wumengxuan0728@gmail.com>
2022-10-14 00:30:48 -0700

    Create MockInterviewFormDetails.jsx

---
+++ components/MockinterviewFormData.jsx
@@ -0,0 +1,20 @@
+const mockInterviewFormDetails = {
+  name: "Ashley Kim",
+  email: "akim291@ucr.edu",
+  typeOfInterview: "technical",
+  formatOfInterview: "in - person",
+  preferredLanguages: ["c++", "python", "javascript"],
+  typesOfProblems: [
+    "graphs",
+    "dynamic programming",
+    "greedy algorithms",
+    "divide & conquer",
+  ],
+  dayTimePlace: {
+    date: "october 26, 2022(wednesday)",
+    time: "11am",
+    location: "wch 127",
+  },
+  intervierwName: ["divyank shah"],
+};
+export default mockInterviewFormDetails;
--- pages/forms.js
+++ pages/forms.js
@@ -2,16 +2,18 @@ import React from "react";
 import AvailableForms from "../components/AvailableForms";
 import CompletedForms from "../components/CompletedForms";
 import SubmittedForms from "../components/SubmittedForms";
+import MockInterviewFormDetails from "../components/MockInterviewFormDetails";
 
 const forms = () => {
   return (
-    <>
+    <div className="pt-[14vh]">
+      <MockInterviewFormDetails />
       <AvailableForms />
       <br></br>
       <SubmittedForms />
       <br></br>
       <CompletedForms />
-    </>
+    </div>
   );
 };
 
