44ea8f47a41edd0ec673a37f908c6ffcb79843a4
Divyank Shah <divyank.shah.2016@gmail.com>
2022-10-15 21:45:34 -0700

    setup auth

--- firebase.js
+++ firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,4 +20,5 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
 // export const analytics = getAnalytics(app);
