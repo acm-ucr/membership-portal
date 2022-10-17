3d1af07232d0fd6067f658ed296c897820301dfe
Kninja99 <kcdamschen@yahoo.com>
2022-10-16 18:14:25 -0700

    time menu drop down styled
    * styling for majority of the component
    * arrow for the drop down is currently not alligned so that still needs
      to be fixed
    * also need to add logic to update the displayed time

--- components/TimeFilter.jsx
+++ components/TimeFilter.jsx
@@ -4,36 +4,24 @@ import Accordion from "react-bootstrap/Accordion";
 const TimeFilter = () => {
   return (
     <div>
-      <Accordion className="w-72" defaultActiveKey="0">
-        <Accordion.Item eventKey="0">
-          <Accordion.Button
-            className="!bg-acm-black !text-acm-white after:!bg-none"
-            id="selected"
-          >
-            Last Month
+      <Accordion
+        className="w-60 text-2xl font-semibold"
+        defaultActiveKey="none"
+      >
+        <Accordion.Item className="border-0" eventKey="0">
+          <Accordion.Button className="!flex h-12 mb-[-20px] !bg-acm-black !text-acm-white after:!bg-[url(/drop-down-arrow.png)] after:self-center !rounded-3xl">
+            <p className="text-2xl m-0">last week</p>
           </Accordion.Button>
-          <Accordion.Body
-            className="text-center bg-red-500 border-acm-black border-y border-t-0 border-x-[3px] "
-            id="today-selcet"
-          >
+          <Accordion.Body className="ml-auto mr-auto w-56 flex items-center justify-center h-16 !pt-[30px] text-center border-acm-black border-y border-t-0 border-x-[3px] cursor-pointer hover:bg-acm-blue">
             today
           </Accordion.Body>
-          <Accordion.Body
-            className="text-center border-acm-black border-y border-x-[3px]"
-            id="last-week-select"
-          >
+          <Accordion.Body className="ml-auto mr-auto w-56 flex items-center justify-center h-12 text-center border-acm-black border-y border-x-[3px] cursor-pointer hover:bg-acm-blue">
             last week
           </Accordion.Body>
-          <Accordion.Body
-            className="text-center border-acm-black border-y border-x-[3px]"
-            id="last-month-select"
-          >
+          <Accordion.Body className="ml-auto mr-auto w-56 flex items-center justify-center h-12 text-center border-acm-black border-y border-x-[3px] cursor-pointer hover:bg-acm-blue">
             last month
           </Accordion.Body>
-          <Accordion.Body
-            className="text-center border-acm-black border-y border-b-2 border-x-[3px]"
-            id="last-year-select"
-          >
+          <Accordion.Body className="ml-auto mr-auto w-56 flex items-center justify-center h-12 text-center border-acm-black border-y border-b-2 border-x-[3px] cursor-pointer hover:bg-acm-blue rounded-b-3xl">
             last year
           </Accordion.Body>
         </Accordion.Item>
