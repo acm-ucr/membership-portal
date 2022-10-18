4c8ee6cccb1c0263aaa8a9325d343ba3eb478fdc
Arnav-Menon <arnavmenon@gmail.com>
2022-10-18 13:01:03 -0700

    files for ResumeReview functionality

---
+++ components/ResumeReviewFormData.jsx
@@ -0,0 +1,13 @@
+const ResumeReviewFormData = {
+    name: "nav",
+    email: "nunyabizness@outlook.com",
+    formatOfInterview: "in - person",
+    dayTimePlace: {
+        date: "october 31, 2022 (monday)",
+        time: "1pm",
+        location: "wch127",
+    },
+    interviewerName: ["nav"]
+};
+
+export default ResumeReviewFormData;
