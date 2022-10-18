383de66ac9e605c86cebb20d5ba712bc6e8cc855
Arnav-Menon <arnavmenon@gmail.com>
2022-10-18 13:00:32 -0700

    added ResumeReviewFormDetails component for testing

--- pages/forms.js
+++ pages/forms.js
@@ -5,11 +5,14 @@ import SubmittedForms from "../components/SubmittedForms";
 //  import MockInterviewForm from "../components/MockInterviewForm";
 import MockInterviewFormData from "../components/MockinterviewFormData";
 import MockInterviewFormDetails from "../components/MockInterviewFormDetails";
+// import ResumeReviewForm from "../components/ResumeReviewForm";
+import ResumeReviewFormData from "../components/ResumeReviewFormData";
+import ResumeReviewFormDetails from "../components/ResumeReviewFormDetails";
 
 const forms = () => {
   return (
     <div className="pt-[14vh]">
-      <MockInterviewFormDetails
+      {/* <MockInterviewFormDetails
         name={MockInterviewFormData.name}
         email={MockInterviewFormData.email}
         typeOfInterview={MockInterviewFormData.typeOfInterview}
@@ -18,7 +21,16 @@ const forms = () => {
         preferredLanguages={MockInterviewFormData.preferredLanguages}
         typesOfProblems={MockInterviewFormData.typesOfProblems}
         intervierwName={MockInterviewFormData.intervierwName}
+      /> */}
+      <br></br>
+      <ResumeReviewFormDetails
+        name={ResumeReviewFormData.name}
+        email={ResumeReviewFormData.email}
+        formatOfInterview={ResumeReviewFormData.formatOfInterview}
+        dayTimePlace={ResumeReviewFormData.dayTimePlace}
+        interviewerName={ResumeReviewFormData.interviewerName}
       />
+      <br></br>
       <AvailableForms />
       <br></br>
       <SubmittedForms />
