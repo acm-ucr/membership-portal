dcf44472a749a48d605a38d0cda47199c55feeba
Jyro888 <92472721+Jyro88@users.noreply.github.com>
2022-10-26 15:27:16 -0700

    Move Photo Code to Photo.jsx

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import { Container} from "react-bootstrap";
+import Image from "next/image";
+
+const images = ["/1662187462679.png", "/fake-qr.png"];
 
 const Photo = () => {
-  return <div>Photo</div>;
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [touchStart, setTouchStart] = useState(null);
+  const [touchEnd, setTouchEnd] = useState(null);
+  const minSwipeDistance = 30;
+
+  const goToNext = () => {
+    const isLastSlide = currentIndex === images.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+  const onTouchStart = (e) => {
+    setTouchEnd(null); // otherwise the swipe is fired even with usual touch events
+    setTouchStart(e.targetTouches[0].clientX);
+  };
+
+  const onTouchMove = (e) => setTouchEnd(e.targetTouches[0].clientX);
+
+  const onTouchEnd = () => {
+    if (!touchStart || !touchEnd) return;
+    const distance = touchStart - touchEnd;
+    const isLeftSwipe = distance > minSwipeDistance;
+    const isRightSwipe = distance < -minSwipeDistance;
+    if (isRightSwipe || isLeftSwipe) {
+      goToNext;
+    }
+  };
+  return(
+    <Container
+        onClick={goToNext}
+        onTouchStart={onTouchStart}
+        onTouchMove={onTouchMove}
+        onTouchEnd={onTouchEnd}
+      >
+    <Image
+    src={images[currentIndex]}
+    alt="som"
+    width={500}
+    height={500}
+  />
+  </Container>
+  );
 };
 
 export default Photo;
