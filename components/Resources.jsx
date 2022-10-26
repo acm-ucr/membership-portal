17003cc3b9d9e2afaf147f78a341dff69cd2bb3a
wmx-OwO <wumengxuan0728@gmail.com>
2022-10-26 14:57:14 -0700

    resources api call

--- components/Resources.jsx
+++ components/Resources.jsx
@@ -1,47 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import Resource from "./Resource";
 import TimeFilter from "./TimeFilter";
 import { Col, Row } from "react-bootstrap";
+import { useEffect } from "react";
+import axios from "axios";
 
-const recourceCardList = [
-  {
-    title: "API Workshop",
-    documentlink: "https://www.youtube.com/watch?v=w7ejDZ8SWv8",
-    youtubelink: "https://www.youtube.com/watch?v=w7ejDZ8SWv8",
-    gitlink: "https://www.youtube.com/watch?v=w7ejDZ8SWv8",
-  },
-  {
-    title: "Professional Development Workshop",
-    documentlink: "https://www.youtube.com/watch?v=w7ejDZ8SWv8",
-    youtubelink: "https://www.youtube.com/watch?v=w7ejDZ8SWv8",
-    gitlink: "",
-  },
-  {
-    title: "Web Development Workshop",
-    documentlink: "https://www.youtube.com/watch?v=w7ejDZ8SWv8",
-    youtubelink: "https://www.youtube.com/watch?v=w7ejDZ8SWv8",
-    gitlink: "https://www.youtube.com/watch?v=w7ejDZ8SWv8",
-  },
-  {
-    title: "Web Development Workshop",
-    documentlink: "https://www.youtube.com/watch?v=w7ejDZ8SWv8",
-    youtubelink: "https://www.youtube.com/watch?v=w7ejDZ8SWv8",
-    gitlink: "https://www.youtube.com/watch?v=w7ejDZ8SWv8",
-  },
-];
+// const recourceCardList = [
+//   {
+//     title: "API Workshop",
+//     documentlink: "https://www.youtube.com/watch?v=w7ejDZ8SWv8",
+//     youtubelink: "https://www.youtube.com/watch?v=w7ejDZ8SWv8",
+//     gitlink: "https://www.youtube.com/watch?v=w7ejDZ8SWv8",
+//   },
+//   {
+//     title: "Professional Development Workshop",
+//     documentlink: "https://www.youtube.com/watch?v=w7ejDZ8SWv8",
+//     youtubelink: "https://www.youtube.com/watch?v=w7ejDZ8SWv8",
+//     gitlink: "",
+//   },
+//   {
+//     title: "Web Development Workshop",
+//     documentlink: "https://www.youtube.com/watch?v=w7ejDZ8SWv8",
+//     youtubelink: "https://www.youtube.com/watch?v=w7ejDZ8SWv8",
+//     gitlink: "https://www.youtube.com/watch?v=w7ejDZ8SWv8",
+//   },
+//   {
+//     title: "Web Development Workshop",
+//     documentlink: "https://www.youtube.com/watch?v=w7ejDZ8SWv8",
+//     youtubelink: "https://www.youtube.com/watch?v=w7ejDZ8SWv8",
+//     gitlink: "https://www.youtube.com/watch?v=w7ejDZ8SWv8",
+//   },
+// ];
 
 const Resources = () => {
+  const [resources, setResources] = useState([]);
+  useEffect(() => {
+    axios.get("/api/getAllResources")
+    .then(
+      response =>{
+        setResources(response.data)
+        console.log(response.data[0].data)
+      }
+    )
+    .catch(error=>{
+      console.log(error)
+    })
+  }, []);
+
   return (
     <div className="w-11/12 flex items-center flex-col justify-center">
       <TimeFilter />
       <Row className="w-11/12 items-center !mt-16">
-        {recourceCardList.map((recourceCard, index) => (
+        {resources.map((recourceCard, index) => (
           <Col className="p-3" xs={12} sm={6} md={4} lg={3} key={index}>
             <Resource
               titles={recourceCard.title}
-              documentLink={recourceCard.documentlink}
-              youtubeLink={recourceCard.youtubelink}
-              githubLink={recourceCard.gitlink}
+              documentLink={recourceCard.data.slides}
+              youtubeLink={recourceCard.data.youtube}
+              githubLink={recourceCard.data.github}
             />
           </Col>
         ))}
