935b96a9dbc55aece2e16c7a3d57ad73b1842ab5
Jyro888 <92472721+Jyro88@users.noreply.github.com>
2022-11-02 14:01:08 -0700

    Made QR Code Button

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Container } from "react-bootstrap";
+import { Row, Col, Container } from "react-bootstrap";
 import Image from "next/image";
 
 const images = ["/1643607679196.png", "/fake-qr.png"];
@@ -32,14 +32,24 @@ const Photo = () => {
     }
   };
   return (
-    <Container
-      onClick={goToNext}
-      onTouchStart={onTouchStart}
-      onTouchMove={onTouchMove}
-      onTouchEnd={onTouchEnd}
-    >
-      <Image src={images[currentIndex]} alt="som" width={500} height={500} />
-    </Container>
+    <Row className="w-full">
+      <Container>
+        <Image src={images[currentIndex]} alt="som" width={500} height={500} />
+      </Container>
+      <Row>
+        <Col xl={5}>
+          <button
+            className=" w-full mt-3 py-2 font-lexend font-bold text-acm-black border-2 border-acm-lightpurple text-2xl transition-colors duration-150 bg-acm-lightpurple rounded-lg focus:shadow-outline hover:border-acm-black "
+            onClick={goToNext}
+            onTouchStart={onTouchStart}
+            onTouchMove={onTouchMove}
+            onTouchEnd={onTouchEnd}
+          >
+            QR Code/Profile Picture
+          </button>
+        </Col>
+      </Row>
+    </Row>
   );
 };
 
