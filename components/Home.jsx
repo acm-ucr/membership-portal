72d1a8b1e87ee352cfb4e474a6691c97bcf808f2
Kninja99 <kcdamschen@yahoo.com>
2022-11-09 09:38:33 -0800

    point addition and timeFilter eslint error fix
    * added points to the home component to render after api call
    * set a default state values if api call fails
    * changed timeFilter to use a state because it was failing eslint for
      some reason

--- components/Home.jsx
+++ components/Home.jsx
@@ -2,13 +2,19 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Home = ({ name, points, year }) => {
-  const [userInfo, setUserInfo] = useState({});
-
+  //  default state
+  const [userInfo, setUserInfo] = useState({
+    name: "User",
+    points: 0,
+    year: 2022,
+  });
+  //  api call to get user data
   useEffect(() => {
     axios
       .get("/api/profile/getInfo")
       .then((response) => {
         setUserInfo(response.data);
+        console.log(response.data);
       })
       .catch((error) => {
         console.log(error);
@@ -19,14 +25,12 @@ const Home = ({ name, points, year }) => {
     <div className="ml-12">
       <div className="mb-4">
         <h1 className="text-8xl text-black font-bold h-min w-min">welcome‚</h1>
-        <h1 className="text-8xl text-black font-bold">
-          {userInfo.name ? userInfo.name : name}
-        </h1>
+        <h1 className="text-8xl text-black font-bold">{userInfo.name}</h1>
       </div>
 
       <div>
         <h5 className="text-4xl text-acm-gray font-acm ">
-          member since {year} | {points} points
+          member since {year} | {userInfo.points} points
         </h5>
       </div>
     </div>
