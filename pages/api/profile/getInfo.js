b16bf70e0c6d684c2a345e47dedbfe7a19b35640
Menthy <wumengxuan0728@gmail.com>
2023-01-08 20:41:22 -0800

    Change to use email from auth

--- pages/api/profile/getInfo.js
+++ pages/api/profile/getInfo.js
@@ -2,7 +2,7 @@ import { db } from "../../../firebase";
 import { doc, getDoc } from "firebase/firestore";
 
 export default async function getProfileInfo(req, res) {
-  const docSnap = await getDoc(doc(db, "users", "rzaid003@ucr.edu"));
+  const docSnap = await getDoc(doc(db, "users", req.body.email));
 
   if (!docSnap.exists()) {
     res.status(400).json({});
