e44aac715fe029d5766c6b39b2fc5b9ef3e108e6
SuryatejaDuvvuri <duvvurisuryateja95@gmail.com>
2023-01-30 18:51:24 -0800

    display background header colors

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = ({ title, color }) => {
   return (
     <div
-      className={`w-11/12 p-2 font-lexend font-semibold rounded-3 text-2xl bg-${color} text-acm-white`}
+      className={`w-11/12 p-2 font-lexend font-semibold rounded-3 text-2xl ${color}  text-acm-white`}
     >
       {title}
     </div>
