8db358063e5add63339ee7ca023e39839db1f8af
swooshoo <102700240+swooshoo@users.noreply.github.com>
2023-02-10 16:57:24 -0800

    issue 201 making headers same size

--- components/Meetings.jsx
+++ components/Meetings.jsx
@@ -20,7 +20,7 @@ const Meetings = () => {
   {
     return (
       <div className="flex flex-col font-lexend border-black border-2 rounded-3xl">
-        <div className="flex bg-black text-white text-2xl font-semibold rounded-3xl p-1 px-4 w-fit mx-4 mt-4 mb-4">
+        <div className="inline-block bg-black text-white text-3xl font-semibold rounded-3xl py-2 px-3 w-fit mx-4 mt-4 mb-4">
           scheduled meetings
         </div>
         {MeetingsData.map((meeting, value) => (
