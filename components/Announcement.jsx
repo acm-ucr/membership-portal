297f6914e4fb033f98a1b72698c64b9a98704eb1
Kninja99 <kcdamschen@yahoo.com>
2023-02-20 09:36:39 -0800

    Bullet point fix
    * added a min width and height to bullet point to prevent to flex box
      from resizing it smaller then it should be
    * also added date, time, and location in p tags as they weren't in any
      tags previously. this will make them easier to style if we need to
      later on

--- components/Announcement.jsx
+++ components/Announcement.jsx
@@ -19,21 +19,21 @@ const Announcement = ({
         <p className="text-3xl font-semibold mx-2">{title}</p>
         <div className="flex justify-start items-center text-xl">
           <FaCircle
-            className={`${text} mx-2 w-2 h-2 sm:w-3 sm:h-3 md:w-4 md:h-4`}
+            className={`${text} mx-2 min-h-[7.5px] min-w-[7.5px] w-2 h-2 sm:w-3 sm:h-3 md:w-4 md:h-4`}
           />
-          date: {date}
+          <p className="m-0">date: {date}</p>
         </div>
         <div className="flex justify-start items-center text-xl">
           <FaCircle
-            className={`${text} mx-2 w-2 h-2 sm:w-3 sm:h-3 md:w-4 md:h-4`}
+            className={`${text} mx-2 min-h-[7.5px] min-w-[7.5px] w-2 h-2 sm:w-3 sm:h-3 md:w-4 md:h-4`}
           />
-          time: {time}
+          <p className="m-0">time: {time}</p>
         </div>
         <div className="flex justify-start items-center text-xl">
           <FaCircle
-            className={`${text} mx-2 w-2 h-2 sm:w-3 sm:h-3 md:w-4 md:h-4`}
+            className={`${text} mx-2 min-h-[7.5px] min-w-[7.5px] w-2 h-2 sm:w-3 sm:h-3 md:w-4 md:h-4`}
           />
-          location: {location}
+          <p className="m-0">location: {location}</p>
         </div>
       </div>
     </div>
