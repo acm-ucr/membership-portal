fb9454dbbc1ae68d4db1e16b9ec34a07f077904a
achen409 <andrewr.chen@gmail.com>
2023-05-21 11:22:36 -0700

    deleted select code in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -13,30 +13,6 @@ function MyApp({ Component, pageProps }) {
   const [events, setEvents] = useState([]);
   const [announcements, setAnnouncements] = useState([]);
 
-  const colorMappings = {
-    social: "!bg-acm-red",
-    career: "!bg-acm-lightpurple",
-    general: "!bg-acm-blue",
-    technical: "!bg-acm-orange",
-    academic: "!bg-acm-marine",
-  };
-
-  const colorMappingsText = {
-    social: "text-acm-red",
-    career: "text-acm-lightpurple",
-    general: "text-acm-blue",
-    technical: "text-acm-orange",
-    academic: "text-acm-marine",
-  };
-
-  const colorMappingsBorder = {
-    social: "border-acm-red",
-    career: "border-acm-lightpurple",
-    general: "border-acm-blue",
-    technical: "border-acm-orange",
-    academic: "border-acm-marine",
-  };
-
   useEffect(() => {
     axios
       .get("/api/getAllResources")
