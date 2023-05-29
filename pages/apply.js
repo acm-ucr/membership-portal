201339a11f607a3a3494f89892251fed2717559f
theo <theobfernandez@gmail.com>
2023-05-29 13:40:30 -0700

    Redesign apply page

--- pages/apply.js
+++ pages/apply.js
@@ -1,14 +1,14 @@
 import { onAuthStateChanged } from "firebase/auth";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { auth } from "../firebase";
-import ApplicationProfile from "../components/ApplicationProfile";
 import { Col, Row } from "react-bootstrap";
 import axios from "axios";
-import colors from "../components/data/Colors.jsx";
+import { useRouter } from "next/router";
+import PortalContext from "../components/PortalContext";
 
 const ApplyPage = () => {
   const [data, setData] = useState({
-    uid: "",
+    uid: "8",
     name: "",
     email: "",
     year: 2026,
@@ -18,8 +18,14 @@ const ApplyPage = () => {
 
   const thisYear = new Date().getFullYear();
   const years = Array.from(new Array(6), (val, index) => index + thisYear);
+  const router = useRouter();
+
+  const { user } = useContext(PortalContext);
 
   useEffect(() => {
+    if (user) {
+      router.push("/invalid");
+    }
     onAuthStateChanged(auth, (currentUser) => {
       if (currentUser !== null) {
         setData({
@@ -27,7 +33,6 @@ const ApplyPage = () => {
           uid: currentUser.uid,
           email: currentUser.email,
           name: currentUser.displayName,
-          image: currentUser.photoURL,
         });
       }
     });
@@ -41,15 +46,17 @@ const ApplyPage = () => {
   };
 
   const handleSubmit = () => {
-    axios.post("/api/addApplication", {
-      uid: data.uid,
-      name: data.name,
-      email: data.email,
-      major: data.major,
-      year: data.year,
-      response: data.response,
-    });
-    setSubmit(true);
+    if (data.uid && data.name && data.email) {
+      axios.post("/api/addApplication", {
+        uid: data.uid,
+        name: data.name,
+        email: data.email,
+        major: data.major,
+        year: data.year,
+        response: data.response,
+      });
+      setSubmit(true);
+    }
   };
 
   const handleyearChange = (event) => {
@@ -59,36 +66,95 @@ const ApplyPage = () => {
     });
   };
 
+  const handleNameChange = (event) => {
+    setData({
+      ...data,
+      name: event.target.value,
+    });
+  };
+  const handleEmailChange = (event) => {
+    setData({
+      ...data,
+      email: event.target.value,
+    });
+  };
+
   const [count, setCount] = useState(300);
   const [submit, setSubmit] = useState(false);
 
   return (
     <div className="pt-20 h-screen w-screen flex justify-center items-start">
       <div className="w-11/12">
-        <p className="font-lexend text-4xl lg:text-6xl text-black text-center my-4 font-semibold">
-          ACM Member Application
+        <p className="font-lexend text-4xl lg:text-6xl text-black my-4 font-semibold">
+          become a member
         </p>
-        <Row className="flex justify-center w-full items-center">
-          <Col md={4} sm={6}>
-            <ApplicationProfile
-              color={colors[1]}
-              name={data.name}
-              email={data.email}
-              image={data.image}
-            />
-          </Col>
+        <Row className="text-acm-white p-3 py-4 flex justify-center w-full bg-acm-black rounded-3xl items-center">
           <Col className="w-full flex justify-evenly items-start flex-col">
             <div className="w-full flex justify-center items-center flex-col">
-              <Row className="w-11/12">
+              <Col className="w-11/12">
                 <Col
-                  xl={6}
-                  className="flex justify-center items-start flex-col"
+                  xl={11}
+                  className="flex mb-2 justify-center items-start flex-col"
                 >
-                  <p className="font-semibold text-2xl lg:text-4xl font-lexend">
-                    Major
+                  <p className="pl-3 font-semibold text-2xl m-1 lg:text-2xl font-lexend">
+                    name
+                  </p>
+                  <input
+                    className="w-full text-acm-black border-black border-2 py-2 rounded-full px-4 text-xl font-lexend"
+                    type="text"
+                    value={data.name}
+                    onChange={handleNameChange}
+                  />
+                </Col>
+                <Col
+                  xl={11}
+                  className="flex mb-2 justify-center items-start flex-col"
+                >
+                  <p className="pl-3 font-semibold text-2xl m-1 lg:text-2xl font-lexend">
+                    email
+                  </p>
+                  <input
+                    placeholder="netid@ucr.edu"
+                    className="w-full text-acm-black border-black border-2 py-2 rounded-full px-4 text-xl font-lexend"
+                    type="text"
+                    value={data.email}
+                    onChange={handleEmailChange}
+                  />
+                </Col>
+                <Col
+                  xl={11}
+                  className="flex mb-2 justify-center items-start flex-col"
+                >
+                  <p className="pl-3 font-semibold text-2xl m-1 lg:text-2xl font-lexend">
+                    graduating year
                   </p>
                   <select
-                    className="text-acm-black text-lg font-lexend pb-1 bg-white rounded-lg w-full"
+                    type="text"
+                    name="year"
+                    placeholder={data.year}
+                    onChange={handleyearChange}
+                    className="text-acm-black text-lg font-lexend bg-white rounded-full w-full"
+                  >
+                    <option value="none" selected disabled hidden>
+                      Select a Year
+                    </option>
+
+                    {years.map((year) => (
+                      <option key={year.index} value={year}>
+                        {year}
+                      </option>
+                    ))}
+                  </select>
+                </Col>
+                <Col
+                  xl={11}
+                  className="flex justify-center items-start flex-col"
+                >
+                  <p className="pl-3 font-semibold text-2xl m-1 lg:text-2xl font-lexend">
+                    major
+                  </p>
+                  <select
+                    className="text-acm-black text-lg font-lexend bg-white rounded-full w-full"
                     onChange={handleMajorChange}
                   >
                     <option value="none" selected disabled hidden>
@@ -123,31 +189,12 @@ const ApplyPage = () => {
                     </option>
                   </select>
                 </Col>
-                <Col
-                  xl={6}
-                  className="flex justify-center items-start flex-col"
-                >
-                  <p className="font-semibold text-2xl lg:text-4xl font-lexend">
-                    Year
-                  </p>
-                  <select
-                    type="text"
-                    name="year"
-                    placeholder={data.year}
-                    onChange={handleyearChange}
-                    className="text-acm-black text-lg font-lexend pb-1 bg-white rounded-lg w-full"
-                  >
-                    <option value="none" selected disabled hidden>
-                      Select a Year
-                    </option>
-
-                    {years.map((year) => (
-                      <option key={year.index} value={year}>
-                        {year}
-                      </option>
-                    ))}
-                  </select>
-                </Col>
+              </Col>
+            </div>
+          </Col>
+          <Col className="w-full flex justify-evenly items-start flex-col">
+            <div className="w-full flex justify-center items-center flex-col">
+              <Col className="w-11/12">
                 <Col
                   xl={12}
                   className="flex justify-center items-start flex-col mt-4"
@@ -156,11 +203,13 @@ const ApplyPage = () => {
                     <p className="font-semibold text-2xl lg:text-4xl font-lexend text-left">
                       Why do you want to join ACM?
                     </p>
-                    <p className="m-0 font-semibold">{count} Characters Left</p>
+                    <p className="m-0 text-center font-semibold">
+                      {count} Characters Left
+                    </p>
                   </div>
 
                   <textarea
-                    className="w-full rounded resize-none"
+                    className="w-full rounded text-acm-black font-lexend resize-none"
                     maxLength={300}
                     rows="6"
                     onChange={(e) => {
@@ -169,16 +218,32 @@ const ApplyPage = () => {
                     }}
                   />
                 </Col>
-              </Row>
-              <div className="w-11/12 flex justify-end flex-col my-4">
-                <button
-                  onClick={handleSubmit}
-                  className="px-4 py-2 rounded-lg text-lg border-4 border-acm-lightpurple  text-acm-lightpurple hover:bg-acm-lightpurple hover:text-white"
-                >
-                  Submit
-                </button>
+              </Col>
+              <div className="flex  w-11/12 flex-col">
+                <div className="flex mt-4 mb-2">
+                  <button
+                    onClick={handleSubmit}
+                    className="text-2xl w-1/2 overflow-hidden px-10 py-0 font-lexend rounded-full border-4 border-acm-blue bg-acm-blue text-acm-white hover:border-acm-white "
+                  >
+                    sign up
+                  </button>
+
+                  <div className="flex flex-col justify-center w-full">
+                    <p className="font-lexend text-xl p-0 m-0 text-center">
+                      already a member?
+                    </p>
+                    <button
+                      onClick={() => {
+                        router.push("/");
+                      }}
+                      className="font-lexend text-xl p-0 m-0 text-center"
+                    >
+                      sign in
+                    </button>
+                  </div>
+                </div>
                 {submit && (
-                  <p className="font-semibold text-xl text-center my-2">
+                  <p className="m-0 p-0 font-semibold text-center">
                     Thank you for submitting!
                   </p>
                 )}
