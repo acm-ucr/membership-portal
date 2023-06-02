98216a7fd46c307374a7ab17b67147f80338a7da
achen409 <andrewr.chen@gmail.com>
2023-06-02 09:09:48 -0700

    Removed Blue Background

--- components/Clubroom.jsx
+++ components/Clubroom.jsx
@@ -6,7 +6,7 @@ import Frame from "./Frame";
 const Clubroom = () => {
   return (
     <div className="flex justify-center flex-col font-lexend text-acm-black mx-auto w-11/12 h-screen">
-      <Row className="bg-acm-blue flex items-center rounded-2xl !p-4 m-0 pb-8">
+      <Row className="flex items-center rounded-2xl !p-4 m-0 pb-8">
         <Col sm={6} className="text-text w-full h-full p-4 text-2xl ">
           <p>
             We will often hold academic, professional, and technical workshops
@@ -24,7 +24,12 @@ const Clubroom = () => {
           </>
         </Col>
         <Col sm={6} className="flex justify-center items-center w-full p-4">
-          <Frame image="./clubroom.webp" />
+          <Frame
+            image="./clubroom.webp"
+            bottomLeft="#c792ea"
+            topRight="#c3e88d"
+            corners="black"
+          />
         </Col>
       </Row>
     </div>
