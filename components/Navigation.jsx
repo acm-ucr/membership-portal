e1761e986724243998dc3ec2bc974c32f1b91a71
achen409 <andrewr.chen@gmail.com>
2023-06-01 21:44:21 -0700

    Increase logout button padding

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -84,7 +84,7 @@ const Navigation = () => {
               </Link>
               <Link href="/">
                 <p
-                  className="hover:cursor-pointer my-0 px-2 py-1 whitespace-nowrap w-full text-center !text-acm-black !font-medium hover:!text-acm-white bg-acm-gray rounded-xl ml-6"
+                  className="hover:cursor-pointer my-0 px-3 py-2 whitespace-nowrap w-full text-center !text-acm-black !font-medium hover:!text-acm-white bg-acm-gray rounded-xl ml-6"
                   onClick={logout}
                 >
                   logout
