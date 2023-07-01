70cf970abf97cacb5f5826bdf19b8af597287dfc
qweasd602 <53993828+qweasd602@users.noreply.github.com>
2023-07-01 05:30:53 -0700

    Increase the width of clubroom picture

--- components/Frame.jsx
+++ components/Frame.jsx
@@ -4,7 +4,7 @@ import { Image } from "react-bootstrap";
 const Frame = ({ image, bottomLeft, topRight, corners }) => {
   return (
     <div
-      className="p-4 w-9/12 h-9/12"
+      className="p-4 w-12/12 h-9/12"
       style={{
         backgroundImage: `conic-gradient(${topRight} 90deg, ${corners} 90deg, ${corners} 180deg, ${bottomLeft} 180deg, ${bottomLeft} 270deg, ${corners} 0deg)`,
       }}
