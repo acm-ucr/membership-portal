9bcbf318f74c8fde8bc373c5648f496df7f2b443
GLKumfert <gkumf001@ucr.edu>
2023-07-10 00:40:26 -0700

    fixes #431, removed filler text and replaced with attribute from user obeject

--- components/ResumeFeedback.jsx
+++ components/ResumeFeedback.jsx
@@ -1,14 +1,15 @@
 import React from "react";
-
-const feedbackList = ["your", "resume", "kinda", "trash"];
+import PortalContext from "./PortalContext";
 
 const ResumeFeedback = () => {
+  const { user } = useContext(PortalContext);
+  const feedbackList = user.resume_feedback;
+
   return (
     <div className="w-full">
       <p className="text-acm-black text-3xl font-lexend font-bold m-0 pb-1">
         feedback
       </p>
-      <p className="text-acm-black text-xl font-lexend m-0 pb-1">formatting</p>
       <div className="flex w-11/12 flex-grow flex-col border-2 border-acm-black">
         {feedbackList.map((feedback, index) => (
           <li
