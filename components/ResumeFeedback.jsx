209e91f42d85ed5435b3514df431502ccfb08a10
GLKumfert <gkumf001@ucr.edu>
2023-07-10 20:36:04 -0700

    updated null handling

--- components/ResumeFeedback.jsx
+++ components/ResumeFeedback.jsx
@@ -4,8 +4,11 @@ import PortalContext from "./PortalContext";
 
 const ResumeFeedback = () => {
   const { user } = useContext(PortalContext);
-  const feedbackList = user.resume_feedback;
-
+  let feedbackList = ["No Feedback Yet"];
+  if (user != null && { user }.resume_feedback != null){
+    feedbackList = { user }.resume_feedback;
+  }
+  
   return (
     <div className="w-full">
       <p className="text-acm-black text-3xl font-lexend font-bold m-0 pb-1">
