c0fb5898a77c6252e6a64e5096db07b710226a43
Divyank Shah <divyank.shah.2016@gmail.com>
2023-07-14 16:06:49 -0400

    user check on feedback

--- components/ResumeFeedback.jsx
+++ components/ResumeFeedback.jsx
@@ -6,14 +6,16 @@ const ResumeFeedback = () => {
   const { user } = useContext(PortalContext);
 
   return (
-    <div className="w-full">
-      <p className="text-acm-black text-3xl font-lexend font-bold m-0 pb-1">
-        feedback
-      </p>
-      <div className="flex w-11/12 flex-grow flex-col border-2 border-acm-black">
-        {user.feedback ? user.feedback : "No Feedback"}
+    user && (
+      <div className="w-full">
+        <p className="text-acm-black text-3xl font-lexend font-bold m-0 pb-1">
+          feedback
+        </p>
+        <div className="flex w-11/12 flex-grow flex-col border-2 border-acm-black">
+          {user.feedback ? user.feedback : "No Feedback"}
+        </div>
       </div>
-    </div>
+    )
   );
 };
 
