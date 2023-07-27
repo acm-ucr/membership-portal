4df5b922201e589bf20f9487aa2a18fdfa5989e8
Kevin He <53993828+kevinhehee@users.noreply.github.com>
2023-07-26 18:56:34 -0700

    404 page automatically reroutes to /user/dashboard

--- pages/404.js
+++ pages/404.js
@@ -1,11 +1,20 @@
-import PageError from "../components/PageError";
+// import PageError from "../components/PageError"; // ORIGINAL 404 PAGE
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 const Error400 = () => {
-  return (
-    <div className="w-screen h-screen">
-      <PageError errorCode="404" errorMsg="Page Not Found" />
-    </div>
-  );
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/user/dashboard");
+  }, []);
+
+  return null;
+  // return (             // ORIGINAL 404 PAGE
+  //   <div className="w-screen h-screen">
+  //     <PageError errorCode="404" errorMsg="Page Not Found" />
+  //   </div>
+  // );
 };
 
 export default Error400;
