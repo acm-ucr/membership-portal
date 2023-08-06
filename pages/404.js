18e495e3604f4f58277b9181b7f4271d3d8ec53b
Kevin He <53993828+kevinhehee@users.noreply.github.com>
2023-08-06 14:21:08 -0700

    Redirects to /user/dashboard when /user is found in url

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,13 @@
 import PageError from "../components/PageError";
+import { useEffect } from "react"
 
 const Error400 = () => {
+  useEffect(() => {
+    if (window.location.pathname === "/user")
+    {
+      window.location.pathname = "/user/dashboard";
+    }
+  })
   return (
     <div className="w-screen h-screen">
       <PageError errorCode="404" errorMsg="Page Not Found" />
