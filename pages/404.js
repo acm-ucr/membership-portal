15e72defd6bf379ae32892e7d055549080b5fdbb
Kevin He <53993828+kevinhehee@users.noreply.github.com>
2023-08-06 13:55:46 -0700

    Revert "404 page automatically reroutes to /user/dashboard"
    This reverts commit 4df5b922201e589bf20f9487aa2a18fdfa5989e8.

--- pages/404.js
+++ pages/404.js
@@ -1,20 +1,11 @@
-// import PageError from "../components/PageError"; // ORIGINAL 404 PAGE
-import { useEffect } from "react";
-import { useRouter } from "next/router";
+import PageError from "../components/PageError";
 
 const Error400 = () => {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.replace("/user/dashboard");
-  }, []);
-
-  return null;
-  // return (             // ORIGINAL 404 PAGE
-  //   <div className="w-screen h-screen">
-  //     <PageError errorCode="404" errorMsg="Page Not Found" />
-  //   </div>
-  // );
+  return (
+    <div className="w-screen h-screen">
+      <PageError errorCode="404" errorMsg="Page Not Found" />
+    </div>
+  );
 };
 
 export default Error400;
