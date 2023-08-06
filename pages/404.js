e9f9b076e76e66aa305bfd03f571b4ec4ce25ad5
Kevin He <53993828+kevinhehee@users.noreply.github.com>
2023-08-06 14:35:58 -0700

    Revert "Redirects to /user/dashboard when /user is found in url"
    This reverts commit 18e495e3604f4f58277b9181b7f4271d3d8ec53b.

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,6 @@
 import PageError from "../components/PageError";
-import { useEffect } from "react";
 
 const Error400 = () => {
-  useEffect(() => {
-    if (window.location.pathname === "/user") {
-      window.location.pathname = "/user/dashboard";
-    }
-  });
   return (
     <div className="w-screen h-screen">
       <PageError errorCode="404" errorMsg="Page Not Found" />
