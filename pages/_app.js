c10aa2b67325464a4d6652b78caaacba19287a29
Bryce7832 <bryce.hojo@gmail.com>
2023-08-17 22:18:15 -0700

    Still recieving build errors but now there is only one document and app.js as well as the admin api folder being accessible via /pages/api/admin

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Layout from "../components/Layout";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { onAuthStateChanged } from "firebase/auth";
 import { SessionProvider } from "next-auth/react";
+import { auth } from "../firebase";
 import PortalContext from "../components/PortalContext";
 import {
   colorMappings,
@@ -81,6 +83,26 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
       .catch((error) => {
         console.log(error);
       });
+    onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser !== null) {
+        axios
+          .post("/api/getInfo", { uid: currentUser.uid })
+          .then((response) => {
+            const data = response.data.data;
+            const date = new Date(data.start.seconds * 1000);
+            data.start = date.getFullYear();
+            setUser({
+              ...response.data.data,
+              name: currentUser.displayName,
+              uid: currentUser.uid,
+              email: currentUser.email,
+            });
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    });
   }, []);
 
   return (
--- pages/admin/announcements.js
+++ pages/admin/announcements.js
@@ -1,14 +1,10 @@
 import React from "react";
 import Header from "../components/admin/Header";
-import AddAnnouncements from "../components/admin/AddAnnouncements";
-import Announcements from "../components/admin/Announcements";
 
 const announcements = () => {
   return (
     <div className="flex justify-center items-center flex-col mt-24">
       <Header title="Announcements" color="bg-acm-blue" />
-      <AddAnnouncements />
-      <Announcements />
     </div>
   );
 };
