64218c337303fdad98e3d37b9a7935af89ff554b
Bryce7832 <bryce.hojo@gmail.com>
2023-08-17 18:51:15 -0700

    Added environmental variables to the api call. Api call still not working(in progress)

--- components/Calendar.jsx
+++ components/Calendar.jsx
@@ -11,49 +11,58 @@ import axios from "axios";
 const mLocalizer = momentLocalizer(moment);
 
 const CalendarEvents = () => {
+  googleCalenderApiKey = process.env.GOOGLE_CALENDAR_API_KEY;
+  googleCalendarID = process.env.GOOGLE_CALENDAR_EMAIL;
   const url =
-    "https://www.googleapis.com/calendar/v3/calendars/{self.google_calendar_id}/events?key={self.google_calendar_api_key}&{singleEvents=true&orderBy=startTime&timeMin={current_time_str}&maxResults=10}";
-  axios
-    .get(url)
-    .then((data) => console.log(data))
-    .catch((error) => console.error(error));
+    "https://www.googleapis.com/calendar/v3/calendars/{googleCalendarID}/events?key={googleCalendarApiKey}&{singleEvents=true&orderBy=startTime&timeMin={current_time_str}&maxResults=10}";
+  axios.get(url).then(
+    (response) => {
+      const result = response.data;
+      console.log(result);
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
   const { events } = useContext(PortalContext);
   const [modalEvent, setModalEvent] = useState(null);
   return (
-    <div className="mb-2 h-[90vh] w-11/12">
-      <Calendar
-        className="font-lexend m-0 p-0"
-        events={events}
-        localizer={mLocalizer}
-        defaultView="month"
-        views={["month"]}
-        components={{
-          event: CustomEvent,
-          toolbar: CustomToolbar,
-        }}
-        eventPropGetter={(event) => {
-          return { className: `${event.color}` };
-        }}
-        dayPropGetter={(event) => {
-          const bg =
-            new Date(event).toLocaleDateString() ==
-            new Date().toLocaleDateString()
-              ? "!bg-acm-green"
-              : "!bg-acm-white";
-          return {
-            className: `${bg}`,
-            style: {
-              margin: 0,
-              padding: 0,
-            },
-          };
-        }}
-        onSelectEvent={(event) => {
-          setModalEvent(event);
-        }}
-      />
-      <Modal event={modalEvent} setState={setModalEvent} />
-    </div>
+    events && (
+      <div className="mb-2 h-[90vh] w-11/12">
+        <Calendar
+          className="font-lexend m-0 p-0"
+          events={events}
+          localizer={mLocalizer}
+          defaultView="month"
+          views={["month"]}
+          components={{
+            event: CustomEvent,
+            toolbar: CustomToolbar,
+          }}
+          eventPropGetter={(event) => {
+            return { className: `${event.color}` };
+          }}
+          dayPropGetter={(event) => {
+            const bg =
+              new Date(event).toLocaleDateString() ==
+              new Date().toLocaleDateString()
+                ? "!bg-acm-green"
+                : "!bg-acm-white";
+            return {
+              className: `${bg}`,
+              style: {
+                margin: 0,
+                padding: 0,
+              },
+            };
+          }}
+          onSelectEvent={(event) => {
+            setModalEvent(event);
+          }}
+        />
+        <Modal event={modalEvent} setState={setModalEvent} />
+      </div>
+    )
   );
 };
 
