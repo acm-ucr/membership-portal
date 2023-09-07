af6376a8729f25b1a47e16a305f00dafd99f6b8d
Bryce7832 <bryce.hojo@gmail.com>
2023-09-06 22:15:17 -0700

    Added resume boxes to the page

--- components/Admin/Resume/Resume.jsx
+++ components/Admin/Resume/Resume.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Header from "../../Header";
 import ResumeBox from "../Resume/ResumeBox";
+import { Col, Row } from "react-bootstrap";
+
 const Resume = () => {
   return (
     <div className="pt-[10vh] flex flex-col justify-center items-start w-11/12">
@@ -8,7 +10,33 @@ const Resume = () => {
       <p className="text-acm-black text-2xl font-lexend font-semibold m-0 left-auto px-2 pt-3">
         Submited Resumes:
       </p>
-      <ResumeBox />
+      <div className="overscroll-auto">
+        <Row className="px-2">
+          <Col>
+            <ResumeBox />
+          </Col>
+          <Col>
+            <ResumeBox />
+          </Col>
+          <Col>
+            <ResumeBox />
+          </Col>
+          <Col>
+            <ResumeBox />
+          </Col>
+        </Row>
+        <Row className="px-2">
+          <Col xs={3}>
+            <ResumeBox />
+          </Col>
+        </Row>
+      </div>
+      <p className="text-acm-black text-2xl font-lexend font-semibold m-0 left-auto px-2 pt-3">
+        Reviewed Resumes:
+      </p>
+      <Col className="px-2">
+        <ResumeBox />
+      </Col>
     </div>
   );
 };
